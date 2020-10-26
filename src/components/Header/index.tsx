71fc89ff62015892a3165bbf9e73add0fed65059
Stefano Saffran <stefanosaffran@hotmail.com>
2020-10-26 10:18:17 -0400

    :lipstick: chore: add cart Badge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import React, { memo } from 'react';
 
-import { Container, Wrapper, LeftNav, RightNav } from './styles';
+import { FiShoppingBag } from 'react-icons/fi';
+
+import { Container, Wrapper, LeftNav, RightNav, Badge } from './styles';
 import Input from '../Input';
+import Gradient from './Gradient';
 
 import { Mission } from '~/assets';
+
+function handleOpenCart() {
+  // TODO - Chamar a pagina do carrinho.
+}
 
 const Header = () => (
   <Container>
@@ -19,9 +26,13 @@ const Header = () => (
           icon
           placeholder="O que você procura?"
         />
-        <button type="button">C</button>
+        <Badge type="button" onClick={handleOpenCart} hasProducts={2}>
+          <FiShoppingBag size={26} style={{ marginLeft: 15 }} />
+          <sup>{3}</sup>
+        </Badge>
       </RightNav>
     </Wrapper>
+    <Gradient />
   </Container>
 );
 
